d364e0e5d0174b33d61bb35c2059cba4b593a0c9
ReddyPrashanth <s.prasantreddy94@gmail.com>
2022-01-19 22:51:50 -0600

    added contact me form

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('portfolio/contact/', views.contact, name='contact'),
     path('portfolio/', views.portfolio, name='portfolio')
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import re
+import json
 
 # Create your views here.
 
@@ -7,3 +10,12 @@ def home(request):
 
 def portfolio(request):
     return render(request, 'core/portfolio.html')
+
+def contact(request):
+    data = json.loads(request.body)
+    if not (bool(data.get('name', False)) and bool(data.get('email', False)) and bool(data.get('message', False))):
+        return JsonResponse({'error': 'All fields are required.'}, status=400)
+    email = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if(not email.match(data['email'])):
+        return JsonResponse({'error': 'Email is not valid. Provide a valid email address.'}, status=400)
+    return JsonResponse(request.POST, status=200)
